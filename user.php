0ece62779993e854858240606cad18b97ebb95b2
tonyito <tonyitocole@gmail.com>
2017-01-27 15:52:24 -0800

    htdocs
    synced

--- user.php
+++ user.php
@@ -7,7 +7,7 @@ if ($connuser->connect_error) {
     die("Connection failed: " . $connuser->connect_error);
 }
 // Select username
-$selected_user = $connuser->query("SELECT * FROM usernames WHERE name='tonito'");
+$selected_user = $connuser->query("SELECT * FROM users WHERE name='tonito'");
 while ($userrow = $selected_user->fetch_assoc()){
 $user= $userrow['name'];
 }
@@ -28,24 +28,41 @@ $selected_rating = $connrating->query("SELECT * FROM rating WHERE for_user='$use
 echo "<body>
       <h1>$user's Profile</h1>
       ";
-while ($ratingrow = $selected_rating->fetch_assoc()){
-  echo "<p>Rating by $ratingrow[by_user]: $ratingrow[rate], $ratingrow[comment]</p>";
+if ($selected_rating->num_rows > 0){
+  while ($ratingrow = $selected_rating->fetch_assoc()){
+    echo "<p>Rating by $ratingrow[by_user]: $ratingrow[rating], $ratingrow[comment]</p>";
+  }
 }
-$average_rating = $connrating->query("SELECT AVG(rate) AS average_rate FROM rating WHERE for_user='$user'");
+else {
+  echo "No ratings";
+}
 
-while ($averagerow = $average_rating->fetch_assoc()){
-  echo "<h2>This user's average rating is: $averagerow[average_rate]</h2>";
-}
+$average_rating = $connrating->query("SELECT AVG(rating) AS average_rate FROM rating WHERE for_user='$user'");
+  while ($averagerow = $average_rating->fetch_assoc()){
+      if ($averagerow['average_rate'] > 0) {
+        echo "<h2>This user's average rating is: $averagerow[average_rate]</h2>";
+      }
+      else {
+        echo "<p>This user has no ratings.</p>";
+      }
+  }
+
 $connrating->close();
 echo "<h3>$user's Active Ads:</h3>";
 $connads = new mysqli('localhost',$config['username'],$config['password'],'ads');
 if ($connads->connect_error) {
     die("Connection failed: " . $connads->connect_error);
 }
-$selected_ad = $connads->query("SELECT * FROM ads WHERE name='$user'");
+$selected_ad = $connads->query("SELECT * FROM ads WHERE user='$user'");
+if ($selected_ad->num_rows > 0){
 while ($adrow = $selected_ad->fetch_assoc()){
   echo "<a href='#'>$adrow[title]: \$$adrow[price]</a>";
 }
+}
+else {
+  echo "This user has no active advertisements.";
+}
+
 echo
       "
       </body>
